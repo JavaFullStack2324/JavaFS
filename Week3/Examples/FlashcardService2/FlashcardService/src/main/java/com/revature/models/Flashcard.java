ada54a03ae08d97a13e7d62879bbc8b9d80bf22c
WilliamTRev <william.terry@revature.com>
2025-11-25 09:31:21 -0500

    updated flashcard2

--- Week3/Examples/FlashcardService2/FlashcardService/src/main/java/com/revature/models/Flashcard.java
+++ Week3/Examples/FlashcardService2/FlashcardService/src/main/java/com/revature/models/Flashcard.java
@@ -1,13 +1,6 @@
 package com.revature.models;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 
 @Entity
