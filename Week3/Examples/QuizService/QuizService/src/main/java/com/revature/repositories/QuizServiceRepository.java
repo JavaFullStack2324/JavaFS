a4fe6790c1a36a2e613029cb86506d73e7f06268
WilliamTRev <william.terry@revature.com>
2025-11-25 10:19:24 -0500

    using full microservice example

--- Week3/Examples/QuizService/QuizService/src/main/java/com/revature/repositories/QuizServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.revature.repositories;
-
-import com.revature.models.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizServiceRepository extends JpaRepository<Quiz,Integer> {
-}
