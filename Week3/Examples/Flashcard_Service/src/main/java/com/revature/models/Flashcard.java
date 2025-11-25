d613b55e2e13c6542c8106a9af4ef4c0f22d3fb2
WilliamTRev <william.terry@revature.com>
2025-11-25 09:39:24 -0500

    updated with old version

--- Week3/Examples/Flashcard_Service/src/main/java/com/revature/models/Flashcard.java
+++ Week3/Examples/Flashcard_Service/src/main/java/com/revature/models/Flashcard.java
@@ -14,7 +14,7 @@ import javax.persistence.ManyToOne;
 public class Flashcard {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	private String question;
